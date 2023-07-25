16df8f0fcadf325bf6c3ba1c7eb5a58f41e035ab
jeeban <jeebanjyoti193@gmail.com>
2023-07-25 21:25:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace MainProject_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string path = "D:\\C# Programme\\MainProject\\MainProject_1\\MainProject_1\\";
+                Console.WriteLine("Enter file name to read out form");
+                string fname = Console.ReadLine();
+                string fpath = path + fname;
+                if (File.Exists(fpath))
+                {
+                    string[] lines = File.ReadAllLines(fpath);
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else { Console.WriteLine("File does not exist"); }
+
+
+            }
+            catch (Exception ex)
+
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
